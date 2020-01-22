46813cbc63c9823aa5cd563440e8200380fb2e73
Grzegorz Sakowski <you@example.com>
2020-01-22 17:34:53 +0100

    002 - utworzenie modelu Order(wstepny formularz zamowienia oraz edycja strony admina

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = ('data', 'godzina')
+    list_display = (Order.order_display, 'data')
+    search_fields = ('data', 'trasa')
+
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    imię = models.CharField(max_length=128)
+    nazwisko = models.CharField(max_length=128)
+    data = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    godzina = models.TimeField(null=True, blank=True)
+    trasa = models.TextField(default='')
+    sprzęt = models.TextField(default='')
+
+    def order_display(self):
+        return 'Zamówienie: {} {}'.format(self.imię, self.nazwisko)
+
+    def __str__(self):
+        return self.order_display()
+
