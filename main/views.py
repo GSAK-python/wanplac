a6911f2edbc1cd8b0907d01629c96cb942f79237
Grzegorz Sakowski <you@example.com>
2020-01-22 18:45:23 +0100

    003 - dodanie templates: szablonu oraz strony z lista rezerwacji, wstepna stylizacja

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Order
+
 
 # Create your views here.
+
+def order(request):
+    rezerwacje = Order.objects.all()
+    return render(request, 'rezerwacje.html', {'rezerwacje': rezerwacje})
