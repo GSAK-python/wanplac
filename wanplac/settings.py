b26a39c32b828bdd3e438d193ab2f4ea8ece409f
Grzegorz Sakowski <you@example.com>
2020-01-23 14:30:29 +0100

    004 - stworzenie strony login.html oraz pliku static moj.css

--- wanplac/settings.py
+++ wanplac/settings.py
@@ -109,3 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'moje_static')
+
+LOGIN_URL = 'login'
+
+LOGOUT_REDIRECT_URL = 'login'
