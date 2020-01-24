1ff03a31140bad27b7021faf112289eacd77f9d5
Grzegorz Sakowski <you@example.com>
2020-01-24 13:13:42 +0100

    008 - dodanie strony do rejestracji

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import order, startowa, kajaki, trasy
+from .views import order, startowa, kajaki, trasy, signup
 
 urlpatterns = [
     path('rezerwacje/', order, name='order'),
     path('startowa/', startowa, name='startowa'),
     path('kajaki/', kajaki, name='kajaki'),
-    path('trasy/', trasy, name='trasy')
+    path('trasy/', trasy, name='trasy'),
+    path('signup/', signup, name='signup')
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order, Startowa
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+
 
 
 @login_required
@@ -20,5 +24,21 @@ def kajaki(request):
 
 def trasy(request):
     return render(request, 'trasy.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('Nazwa użytkownika')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('order')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
 
 
