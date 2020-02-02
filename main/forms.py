50aa3cf60919f98f4be45cea6f836a48b79cda7a
Grzegorz Sakowski <you@example.com>
2020-02-02 17:49:19 +0100

    024 - edycja formluarza do rezerwacji kajakow, zmiana pozycji data, godzina, trasa na listy wyboru

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
+from django import forms
 from .models import MyOrder
 
 
-class OrderForm(ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = MyOrder
-        fields = ['imię', 'nazwisko', 'email', 'data', 'godzina', 'trasa', 'sprzęt']
+        fields = ['imię',  'nazwisko', 'email', 'data', 'godzina', 'trasa', 'sprzęt']
+        help_texts = {
+            'data': 'Maksymalna data rezerwacji wynosi 7 dni od dnia dzisiejszego.',
+            'godzina': 'Możliwość rezerwacji kajaków obowiązuje w godzinach 9:00 - 12:00'
+        }
--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
+from datetime import timedelta
 
 
 class Startowa(models.Model):
@@ -13,11 +15,48 @@ class MyOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     imię = models.CharField(max_length=128)
     nazwisko = models.CharField(max_length=128)
-    data = models.DateField(null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    godzina = models.TimeField(null=True, blank=True)
-    trasa = models.TextField(default='')
+
+    email = models.EmailField()
     sprzęt = models.TextField(default='')
+
+    wybor_dat = {
+        (0, dt.date.today().strftime('%d-%m-%Y')),
+        (1, (dt.date.today() + timedelta(days=+1)).strftime('%d-%m-%Y')),
+        (2, (dt.date.today() + timedelta(days=+2)).strftime('%d-%m-%Y')),
+        (3, (dt.date.today() + timedelta(days=+3)).strftime('%d-%m-%Y')),
+        (4, (dt.date.today() + timedelta(days=+4)).strftime('%d-%m-%Y')),
+        (5, (dt.date.today() + timedelta(days=+5)).strftime('%d-%m-%Y')),
+        (6, (dt.date.today() + timedelta(days=+6)).strftime('%d-%m-%Y'))
+    }
+    wybor_dat_posortowany = sorted(wybor_dat)
+    data = models.IntegerField(choices=wybor_dat_posortowany)
+
+    wybor_tras = {
+        (0, 'Krutyń - Młyn (3km)'),
+        (1, 'Krutyń - Rosocha (6km)'),
+        (2, 'Krutyń - Ukta (13km)'),
+        (3, 'Krutyń - Jezioro Mokre - Krutyń (10km)')
+    }
+    wobor_tras_posortowany = sorted(wybor_tras)
+    trasa = models.IntegerField(choices=wobor_tras_posortowany)
+
+    wybor_godzin = {
+        (0, '9:00'),
+        (1, '9:15'),
+        (2, '9:30'),
+        (3, '9:45'),
+        (4, '10:00'),
+        (5, '10:15'),
+        (5, '10:30'),
+        (7, '10:45'),
+        (8, '11:00'),
+        (9, '11:15'),
+        (10, '11:30'),
+        (11, '11:45'),
+        (12, '12:00')
+    }
+    wybor_godzin_posortowany = sorted(wybor_godzin)
+    godzina = models.IntegerField(choices=wybor_godzin_posortowany)
 
     def order_display(self):
         return 'Zamówienie: {} {}'.format(self.imię, self.nazwisko)
